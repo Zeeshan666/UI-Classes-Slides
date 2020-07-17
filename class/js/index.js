6fdd52544e3bdccf55221ca237bec3d30e25864a
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2020-07-17 23:24:13 +0500

    first commit

---
+++ class/js/index.js
@@ -0,0 +1,18 @@
+// document.write("Variables");
+// alert("hello world");
+// console.log("hy")
+
+ var hello;
+ hello ="hy how are you"
+
+//  var a ;//dec
+//  a=4 //initialize
+//  var b=2;
+//  var c= a / b
+//  document.write(a + " / " + b +" = "+  c )
+var a,b,c,d //dec
+a=5;
+b=4;
+c=6
+d=3
+document.write(a*b-c/d)
